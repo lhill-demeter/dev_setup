379d825e9a861e1ba5c1c0cd013fe0c4ece93bbb
Luther Hill <lhill@demeterlogistics.com>
2021-09-08 09:26:07 -0400

    updated docker install

--- mac_install_dependencies.sh
+++ mac_install_dependencies.sh
@@ -66,7 +66,7 @@ if [[ $? != 0 ]];
 then
     # Install Docker and Docker Desktop
     printf "Installing docker\n\n"
-    brew cask install --cask docker
+    brew cask install docker
     printf "Docker installed\n\n"
 else
     printf "Docker installed\n\n"
